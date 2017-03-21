d2be504c847266ec9109aa16136c7f3fcaaff976
udaykiranreddykondreddy <udaykiran.kondreddy@gmail.com>
2017-03-21 05:12:15 +0000

    Update email.py

--- email.py
+++ email.py
@@ -1,9 +1,9 @@
 import re,urllib2
 emailsearch=re.compile(r'[\w*\.]+@[\w*\.]+')
-files=urllib2.urlopen("http://kluniversity.in/contact.aspx")
+files=urllib2.urlopen("web link")
 code=files.read()
 m=emailsearch.findall(code)
-fopen=open('emails.csv','w')
+fopen=open('file name ','w')
 for i in m:
     fopen.write(i+'\n')
 fopen.close()
